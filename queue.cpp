c7385891a6aab02f04aed9a711c29fd703e33725
Sayeed Joy <sayeedalmahmudjoy@gmail.com>
2025-11-10 12:34:11 +0600

    Nov 10 class: Queue

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,42 @@
-//
-// Created by Sayeed Joy on 9/11/25.
-//
+#include <iostream>
+using namespace std;
+#define SIZE 5
+
+int a[SIZE];
+int f = -1;
+int r = SIZE - 1;
+
+void enqueue(int x) {
+    if (f < SIZE - 1) {
+        f++;
+        a[f] = x;
+    }
+}
+
+void dequeue() {
+    if (r > -1) {
+        r--;
+    }
+}
+
+void print() {
+    for (int i = 0; i <= f && i <= r; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, x;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        enqueue(x);
+    }
+    print();
+    dequeue();
+    print();
+    return 0;
+}
